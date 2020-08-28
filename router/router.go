8792260efda2e89e419343948980d915da41e8fa
Tomi Amao <tomi@simplifyd.com>
2020-08-28 12:42:14 +0100

    updated routes

--- router/router.go
+++ router/router.go
@@ -7,14 +7,12 @@ import (
 
 // Router is exported and used in main.go
 func Router() *mux.Router {
-
 	router := mux.NewRouter()
-
-	router.HandleFunc("/api/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/tasks", middleware.GetAllTask).Methods("GET", "OPTIONS")
+	router.HandleFunc("/tasks", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/tasks/{id}/undo", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/tasks/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/tasks/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
 	return router
 }
